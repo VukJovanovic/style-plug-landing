9662b16008970dac896ba2c282a06f3f135d3d00
vuk <vukjovanovic34@gmail.com>
2019-06-12 17:41:32 +0200

    main form validation

--- js/app.js
+++ js/app.js
@@ -2,8 +2,15 @@ $(document).ready(function () {
     var $form = $('#mc-embedded-subscribe-form')
     if ($form.length > 0) {
         $('form input[type="submit"]').bind('click', function (event) {
-            if (event) event.preventDefault()
-            register($form)
+            event.preventDefault()
+            var testEmail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;;
+            var $emailValue = $('#mce-EMAIL').val();
+            if (testEmail.test($emailValue)) {
+                register($form)
+            }
+            else {
+                $('#subscribe-result').html('<p>Invalid email address</p>')
+            }
         })
     }
 })
@@ -19,23 +26,19 @@ function register($form) {
         contentType: 'application/json; charset=utf-8',
         error: function (err) { alert('Could not connect to the registration server. Please try again later.') },
         success: function (data) {
-            $('#mc-embedded-subscribe').val('subscribe')
+            $('#mc-embedded-subscribe').val('GET PLUGGED IN')
             if (data.result === 'success') {
                 // Yeahhhh Success
-                console.log(data.msg)
                 $('#mce-EMAIL').css('borderColor', '#ffffff')
                 $('#subscribe-result').css('color', '#fff')
                 $('#subscribe-result').css('margin-top', '10px')
                 $('#subscribe-result').html('<p>Thank you for subscribing.</p>')
-                $('#mc-embedded-subscribe').val('GET PLUGGED IN');
             } else {
                 // Something went wrong, do something to notify the user.
-                console.log(data.msg)
                 const msg = data.msg.substring(0, 1);
 
                 // $('#mce-EMAIL').css('borderColor', '#ff8282')
                 $('#subscribe-result').css('color', '#ff8282')
-                $('#mc-embedded-subscribe').val('GET PLUGGED IN');
                 $('#subscribe-result').html('<p>' + msgCheck(msg) + '</p>')
             }
         }
